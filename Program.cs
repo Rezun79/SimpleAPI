06a0c803e17e9cf8bf93818f012d6b6a81359b37
Rezun79 <rezun79@gmail.com>
2025-01-26 17:44:07 +0100

    Test

--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Dodajemy usługę CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()  // Zezwala na dostęp z każdej domeny
+                  .AllowAnyMethod()  // Zezwala na wszystkie metody (GET, POST, itd.)
+                  .AllowAnyHeader(); // Zezwala na wszystkie nagłówki
+        });
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 app.MapGet("/greet", (HttpRequest request) =>
 {
--- obj/Debug/net8.0/SimpleApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/SimpleApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45e7063b28825a42bfd3263512b5ba1cb3fd5633")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4161077e7a8552d09edc6fdfc051af40b2a455d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
